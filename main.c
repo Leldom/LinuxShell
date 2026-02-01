6ab0929bab4bf611b048d9b10cb24d5f458f24bf
Leldom <Kannix1402@gmail.com>
2026-02-01 22:04:50 +0100

    added signal handling

--- main.c
+++ main.c
@@ -2,21 +2,46 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 
-int main() {
+void handleSignal(int sig)
+{
+    (void)sig;
+
+    write(STDOUT_FILENO, "\n", 1);
+
+    write(STDOUT_FILENO, "@MyShell~> ", 11);
+}
+
+int main(int argc, char *argv[]) 
+{
+
+    signal(SIGINT, handleSignal);
+
     char line[MAX_LINE];
     char hostName[256];
     char* args[MAX_ARGS];
 
     gethostname(hostName, sizeof(hostName));
 
-    while (1) {
+    while (1) 
+    {
+
         printf("\033[0;32m%s\033[0m @MyShell~> ", hostName);
 
-        if (!fgets(line, MAX_LINE, stdin)) {
-            break; // Exit on EOF or error
+        if (!fgets(line, MAX_LINE, stdin)) 
+        {
+
+            if(feof(stdin))
+            {
+                printf("\nLogout.\n");
+                break;
+            }
+
+            clearerr(stdin);
+            continue;
         }
 
         line[strcspn(line, "\n")] = 0;
@@ -26,7 +51,8 @@ int main() {
         int i = 0;
         char* token = strtok(line, " ");
 
-        while (token != NULL && i < MAX_ARGS - 1) {
+        while (token != NULL && i < MAX_ARGS - 1) 
+        {
             args[i++] = token;
             token = strtok(NULL, " ");
         }
@@ -34,24 +60,33 @@ int main() {
 
         if (strcmp(args[0], "exit") == 0) break; // Exit the shell
 
-        if (strcmp(args[0], "cd") == 0) {
-            if (args[1] == NULL) {
+        if (strcmp(args[0], "cd") == 0) 
+        {
+            if (args[1] == NULL) 
+            {
                 fprintf(stderr, "cd: missing argument\n");
-            } else {
-                if (chdir(args[1]) != 0) {
+            } else 
+            {
+                if (chdir(args[1]) != 0) 
+                {
                     perror("cd error");
                 }
-                continue;
             }
+            continue;
         }
         const pid_t pid = fork();
-        if (pid < 0) {
+        if (pid < 0) 
+        {
             perror("fork error");
-        } else if (pid == 0) {
+
+        } else if (pid == 0) 
+        {
             execvp(args[0], args);
             perror("command not found");
             return 1;
-        } else {
+            
+        } else 
+        {
             wait(NULL); // Wait for child process to finish
         }
     }
